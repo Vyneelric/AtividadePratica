3bc06439cfcb8a89cde00d76d265e66922115916
Vyneelric <vini.cardoso1909@gmail.com>
2025-10-30 09:47:21 -0300

    FEAT usuario.js

---
+++ model/DAO/usuario.js
@@ -0,0 +1,109 @@
+//Import da biblioteca do prisma cliente para realizar as ações no BD
+const { PrismaClient } = require('@prisma/client')
+const { async } = require('effect/Stream')
+
+//Instância da classe do Prisma Client (cria um objeto)
+const prisma = new PrismaClient()
+
+
+const insertNovoUsuario = async function(usuario) {
+   try {
+       
+    let sql = ` insert into tbl_usuario(
+        login,
+        senha)
+    values
+    ('${usuario.login}',
+     '${usuario.senha}'
+    )
+    `
+
+    let result = await prisma.$executeRawUnsafe(sql)
+
+    if(result)
+        return true
+    else
+        return false
+
+   } catch (error) {
+        return false
+   }
+
+}
+
+const updateNovoUsuario = async function(usuario) {
+    try {
+        
+    let sql = `update tbl_usuario set
+        login =  '${usuario.titulo}',
+        senha = '${usuario.data_publicacao}'
+    where id = '${usuario.id}' `
+
+    
+    let result = await prisma.$executeRawUnsafe(sql)
+
+    if(result)
+        return true
+    else
+        return false
+    } catch (error) {
+        return false
+    }
+    
+}
+
+const deleteUsuario = async function(id) {
+    try {
+        let sql = `delete from tbl_usuario where id = ${id}`
+
+    let result = await prisma.$executeRawUnsafe(sql)
+
+    if(result)
+        return true
+    else
+        return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+
+const selectAllUsuario = async function() {
+    try {
+        let sql = `select * from tbl_usuario order by id desc`
+
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(result)
+            return result
+        else 
+        return false
+    } catch (error) {
+        return false
+    }
+}
+
+const selectByIDUsuario = async function(id) {
+    try {
+        let sql = `select * from tbl_usuario where id = ${id}`
+
+
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(result)
+            return result
+        else
+            return false
+    } catch (error) {
+        return false
+    }
+}
+
+module.exports = {
+    insertNovoUsuario,
+    updateNovoUsuario,
+    deleteUsuario,
+    selectAllUsuario,
+    selectByIDUsuario
+}
